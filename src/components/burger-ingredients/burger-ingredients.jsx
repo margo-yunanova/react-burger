6b0ad8208511179570a0690c7ea7fa051bacd0b0
Margo <m.yunanova@gmail.com>
2023-01-30 16:56:48 +0300

    pre-alpha scroll v1

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import { Counter, Tab, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './burger-ingredients.module.css';
 import {ingredientType} from '../../utils/prop-types';
+import { useEffect, useRef, useState } from 'react';
 
 const Ingredient = ({ ingredient, setCurrentIngredient }) => (
   <div className={styles.cell} onClick={() => setCurrentIngredient(ingredient)}>
@@ -21,6 +22,28 @@ Ingredient.propTypes = {
 }
 
 export default function BurgerIngredients({ ingredients, setCurrentIngredient }) {
+
+  const [activeTab, setActiveTab] = useState('')
+
+  const scrollSection = useRef(null);
+  const titleMeal = useRef(null);
+  const titleSauce = useRef(null);
+  const titleBun = useRef(null);
+
+  const titlesIngredients = [titleBun.current, titleMeal.current, titleSauce.current]
+
+  const getTopCoordinatesActiveTab = (value) => {
+    const { top: topCoordinateScrollSection } = scrollSection.current.getBoundingClientRect();
+    const { top: topCoordinateActiveIngredient} = titlesIngredients.find(ingredient => ingredient.id === value).getBoundingClientRect();
+    return topCoordinateActiveIngredient - topCoordinateScrollSection;
+  }
+
+  const focusTitle = (value) => {
+    setActiveTab(value);
+    scrollSection.current.scrollTo(0, getTopCoordinatesActiveTab(value))
+  }
+
+
   const buns = ingredients.filter(i => i.type === "bun");
   const main = ingredients.filter(i => i.type === "main");
   const sauces = ingredients.filter(i => i.type === "sauce");
@@ -29,27 +52,27 @@ export default function BurgerIngredients({ ingredients, setCurrentIngredient })
     <section className={styles.section}>
       <h1 className="text text_type_main-large pb-5">Соберите бургер</h1>
       <nav className={`${styles.tab} pb-10`}>
-        <Tab>Булки</Tab>
-        <Tab>Соусы</Tab>
-        <Tab>Начинки</Tab>
+        <Tab value='Булки' active={activeTab === 'Булки'} onClick={focusTitle}>Булки</Tab>
+        <Tab value='Соусы' active={activeTab === 'Соусы'} onClick={focusTitle}>Соусы</Tab>
+        <Tab value='Начинки' active={activeTab === 'Начинки'} onClick={focusTitle}>Начинки</Tab>
       </nav>
 
-      <div className={styles.scroll}>
-        <h2 className="text text_type_main-medium pt-6">Булки</h2>
+      <div ref={scrollSection} className={styles.scroll}>
+        <h2 ref={titleBun} id="Булки" className="text text_type_main-medium pt-6">Булки</h2>
         <div className={`${styles.table} pl-4`}>
           {
             buns.map(bun => <Ingredient key={bun._id} ingredient={bun} setCurrentIngredient={setCurrentIngredient}/>)
           }
         </div>
 
-        <h2 className="text text_type_main-medium pt-10 pt-6">Соусы</h2>
+        <h2 ref={titleSauce} id="Соусы" className="text text_type_main-medium pt-10 pt-6">Соусы</h2>
         <div className={`${styles.table} pl-4`}>
           {
             sauces.map(sauce => <Ingredient key={sauce._id} ingredient={sauce} setCurrentIngredient={setCurrentIngredient}/>)
           }
         </div>
 
-        <h2 className="text text_type_main-medium pt-10 pt-6">Начинки</h2>
+        <h2 ref={titleMeal} id="Начинки" className="text text_type_main-medium pt-10 pt-6">Начинки</h2>
         <div className={`${styles.table} pl-4`}>
           {
             main.map(item => <Ingredient key={item._id} ingredient={item} setCurrentIngredient={setCurrentIngredient}/>)
