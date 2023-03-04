02326ec986a6f82153c2f115d55fc58d2fd68433
Margo <m.yunanova@gmail.com>
2023-03-04 19:26:43 +0300

    move main section to HomePage component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,6 @@
 import { useEffect } from 'react';
-import styles from './app.module.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AppHeader from '../app-header/app-header';
-import BurgerIngredients from '../burger-ingredients/burger-ingredients';
-import BurgerConstructor from '../burger-constructor/burger-constructor';
 import OrderDetails from '../order-details/order-details';
 import IngredientDetails from '../ingredient-details/ingredient-details';
 import Modal from '../modal/modal';
@@ -10,40 +8,35 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getIngredients } from '../../services/actions/ingredients';
 import { HIDE_ORDER_MODAL } from '../../services/actions/orderDetails';
 import { HIDE_INGREDIENT_MODAL } from '../../services/actions/current-ingredient';
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { ADD_INGREDIENT_INTO_CONSTRUCTOR } from '../../services/actions/constructor';
-import { v4 as uuidv4 } from 'uuid';
+import Login from "../../pages/login";
+import HomePage from '../../pages/home';
 
 function App() {
 
   const dispatch = useDispatch();
 
-  const ingredients = useSelector(state => state.ingredients.listBurgerIngredients.ingredients);
   const orderDetailVisible = useSelector(state => state.orderDetails.orderDetailVisible);
   const currentIngredientVisible = useSelector(state => state.currentIngredient.currentIngredientVisible);
 
   useEffect(() => dispatch(getIngredients()), [dispatch]);
 
-  const handleDrop = (ingredient) => {
-    dispatch({
-      type: ADD_INGREDIENT_INTO_CONSTRUCTOR,
-      payload: {
-        ingredient,
-        code: uuidv4(),
-      }
-    });
-  };
-
   return (
     <>
       <AppHeader />
-      <main className={styles.menu}>
-        <DndProvider backend={HTML5Backend}>
-          <BurgerIngredients ingredients={ingredients} />
-          {ingredients.length > 0 && <BurgerConstructor onDropHandler={handleDrop} />}
-        </DndProvider>
-      </main>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          {/*
+          <Route path='/register' element={}></Route>
+          <Route path='/forgot-password' element={}></Route>
+          <Route path='/reset-password' element={}></Route>
+          <Route path='/profile' element={}></Route>
+          <Route path='/ingredients/:id' element={}></Route> */}
+        </Routes>
+      </BrowserRouter>
+
+
 
       {currentIngredientVisible &&
         <Modal close={() => dispatch({ type: HIDE_INGREDIENT_MODAL })} title='Детали ингридиента'>
---
+++ src/pages/home.jsx
@@ -0,0 +1,35 @@
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import BurgerIngredients from "../components/burger-ingredients/burger-ingredients";
+import BurgerConstructor from "../components/burger-constructor/burger-constructor";
+import styles from '../components/app/app.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { ADD_INGREDIENT_INTO_CONSTRUCTOR } from '../services/actions/constructor';
+
+const HomePage = () => {
+
+  const ingredients = useSelector(state => state.ingredients.listBurgerIngredients.ingredients);
+  const dispatch = useDispatch();
+
+  const handleDrop = (ingredient) => {
+    dispatch({
+      type: ADD_INGREDIENT_INTO_CONSTRUCTOR,
+      payload: {
+        ingredient,
+        code: uuidv4(),
+      }
+    });
+  };
+
+  return (
+      <main className={styles.menu}>
+        <DndProvider backend={HTML5Backend}>
+          <BurgerIngredients ingredients={ingredients} />
+          {ingredients.length > 0 && <BurgerConstructor onDropHandler={handleDrop} />}
+        </DndProvider>
+      </main>
+  )
+}
+
+export default HomePage;
