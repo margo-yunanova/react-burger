269fd7f06383b724bda004da4726be8c17707159
margo <m.yunanova@gmail.com>
2023-03-13 18:57:21 +0300

    format burger ingredients

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,31 +1,46 @@
-import PropTypes from 'prop-types';
-import { Counter, Tab, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import styles from './burger-ingredients.module.css';
-import { ingredientType } from '../../utils/prop-types';
-import { useRef, useState } from 'react';
-import { SHOW_INGREDIENT_MODAL } from '../../services/actions/current-ingredient';
-import { useDispatch, useSelector } from 'react-redux';
+import {
+  Counter, CurrencyIcon, Tab
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import PropTypes from "prop-types";
+import { useRef, useState } from "react";
 import { useDrag } from "react-dnd";
+import { useDispatch, useSelector } from "react-redux";
+import { SHOW_INGREDIENT_MODAL } from "../../services/actions/current-ingredient";
+import { ingredientType } from "../../utils/prop-types";
+import styles from "./burger-ingredients.module.css";
 
 const Ingredient = ({ ingredient }) => {
-
   const dispatch = useDispatch();
 
-  const { bun, bunFilling } = useSelector(state => state.orderIngredients);
+  const { bun, bunFilling } = useSelector((state) => state.orderIngredients);
 
-  const count = ingredient.type !== 'bun' ? bunFilling.reduce((sum, item) => item._id === ingredient._id ? sum + 1 : sum, 0)
-    : bun?._id === ingredient._id ? 1 : 0;
+  const count =
+    ingredient.type !== "bun"
+      ? bunFilling.reduce(
+          (sum, item) => (item._id === ingredient._id ? sum + 1 : sum),
+          0
+        )
+      : bun?._id === ingredient._id
+      ? 1
+      : 0;
 
   const [{ opacity }, dragRef] = useDrag({
-    type: 'ingredient',
+    type: "ingredient",
     item: ingredient,
-    collect: monitor => ({
-      opacity: monitor.isDragging() ? 0.5 : 1
-    })
+    collect: (monitor) => ({
+      opacity: monitor.isDragging() ? 0.5 : 1,
+    }),
   });
 
   return (
-    <div ref={dragRef} className={styles.cell} style={{ opacity }} onClick={() => dispatch({ type: SHOW_INGREDIENT_MODAL, payload: ingredient })}>
+    <div
+      ref={dragRef}
+      className={styles.cell}
+      style={{ opacity }}
+      onClick={() =>
+        dispatch({ type: SHOW_INGREDIENT_MODAL, payload: ingredient })
+      }
+    >
       <Counter count={count} size="default" extraClass="m-1" />
       <img src={ingredient.image} alt={ingredient.name} />
       <div className={`${styles.price} pt-1 pb-1`}>
@@ -42,20 +57,18 @@ Ingredient.propTypes = {
 };
 
 export default function BurgerIngredients({ ingredients }) {
-
-  const [activeTab, setActiveTab] = useState('Булки');
+  const [activeTab, setActiveTab] = useState("Булки");
 
   const titleMealEl = useRef(null);
   const titleSauceEl = useRef(null);
   const titleBunEl = useRef(null);
 
-
   const scrollToTitle = (activeTab) => {
     setActiveTab(activeTab);
     const titlesIngredients = {
-      'Булки': titleBunEl.current,
-      'Соусы': titleSauceEl.current,
-      'Начинки': titleMealEl.current,
+      Булки: titleBunEl.current,
+      Соусы: titleSauceEl.current,
+      Начинки: titleMealEl.current,
     };
     titlesIngredients[activeTab].scrollIntoView({
       behavior: "smooth",
@@ -63,56 +76,94 @@ export default function BurgerIngredients({ ingredients }) {
   };
 
   const scroll = (evt) => {
-    const numberPixelsScrollSectionMove = evt.target.scrollTop + evt.target.offsetTop;
+    const numberPixelsScrollSectionMove =
+      evt.target.scrollTop + evt.target.offsetTop;
     const topEdgeSauce = titleSauceEl.current.offsetTop;
     const topEdgeMeal = titleMealEl.current.offsetTop;
 
     if (numberPixelsScrollSectionMove < topEdgeSauce) {
-      setActiveTab('Булки');
-    } else if (numberPixelsScrollSectionMove >= topEdgeSauce && numberPixelsScrollSectionMove < topEdgeMeal) {
-      setActiveTab('Соусы');
+      setActiveTab("Булки");
+    } else if (
+      numberPixelsScrollSectionMove >= topEdgeSauce &&
+      numberPixelsScrollSectionMove < topEdgeMeal
+    ) {
+      setActiveTab("Соусы");
     } else {
-      setActiveTab('Начинки');
+      setActiveTab("Начинки");
     }
   };
 
-
-  const buns = ingredients.filter(i => i.type === "bun");
-  const main = ingredients.filter(i => i.type === "main");
-  const sauces = ingredients.filter(i => i.type === "sauce");
+  const buns = ingredients.filter((i) => i.type === "bun");
+  const main = ingredients.filter((i) => i.type === "main");
+  const sauces = ingredients.filter((i) => i.type === "sauce");
 
   return (
     <section className={styles.section}>
       <h1 className="text text_type_main-large pb-5">Соберите бургер</h1>
       <nav className={`${styles.tab} pb-10`}>
-        <Tab value='Булки' active={activeTab === 'Булки'} onClick={scrollToTitle}>Булки</Tab>
-        <Tab value='Соусы' active={activeTab === 'Соусы'} onClick={scrollToTitle}>Соусы</Tab>
-        <Tab value='Начинки' active={activeTab === 'Начинки'} onClick={scrollToTitle}>Начинки</Tab>
+        <Tab
+          value="Булки"
+          active={activeTab === "Булки"}
+          onClick={scrollToTitle}
+        >
+          Булки
+        </Tab>
+        <Tab
+          value="Соусы"
+          active={activeTab === "Соусы"}
+          onClick={scrollToTitle}
+        >
+          Соусы
+        </Tab>
+        <Tab
+          value="Начинки"
+          active={activeTab === "Начинки"}
+          onClick={scrollToTitle}
+        >
+          Начинки
+        </Tab>
       </nav>
 
       <div className={styles.scroll} onScroll={scroll}>
-        <h2 ref={titleBunEl} data-name="Булки" className="text text_type_main-medium pt-6">Булки</h2>
+        <h2
+          ref={titleBunEl}
+          data-name="Булки"
+          className="text text_type_main-medium pt-6"
+        >
+          Булки
+        </h2>
         <div className={`${styles.table} pl-4`}>
-          {
-            buns.map(bun => <Ingredient key={bun._id} ingredient={bun} />)
-          }
+          {buns.map((bun) => (
+            <Ingredient key={bun._id} ingredient={bun} />
+          ))}
         </div>
 
-        <h2 ref={titleSauceEl} data-name="Соусы" className="text text_type_main-medium pt-10 pt-6">Соусы</h2>
+        <h2
+          ref={titleSauceEl}
+          data-name="Соусы"
+          className="text text_type_main-medium pt-10 pt-6"
+        >
+          Соусы
+        </h2>
         <div className={`${styles.table} pl-4`}>
-          {
-            sauces.map(sauce => <Ingredient key={sauce._id} ingredient={sauce} />)
-          }
+          {sauces.map((sauce) => (
+            <Ingredient key={sauce._id} ingredient={sauce} />
+          ))}
         </div>
 
-        <h2 ref={titleMealEl} data-name="Начинки" className="text text_type_main-medium pt-10 pt-6">Начинки</h2>
+        <h2
+          ref={titleMealEl}
+          data-name="Начинки"
+          className="text text_type_main-medium pt-10 pt-6"
+        >
+          Начинки
+        </h2>
         <div className={`${styles.table} pl-4`}>
-          {
-            main.map(item => <Ingredient key={item._id} ingredient={item} />)
-          }
+          {main.map((item) => (
+            <Ingredient key={item._id} ingredient={item} />
+          ))}
         </div>
       </div>
-
     </section>
   );
 }
