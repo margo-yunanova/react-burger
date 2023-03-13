21418273caf87f348619d4dccaa9f9a8ae16794a
margo <m.yunanova@gmail.com>
2023-03-13 20:45:55 +0300

    format modal

--- src/components/modal-overlay/modal-overlay.jsx
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -1,10 +1,15 @@
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router";
 import styles from "./modal-overlay.module.css";
 
 export default function ModalOverlay({ children, close }) {
+
   return (
-    <div className={[styles.popup, styles.opened].join(" ")} onClick={close}>
-      {children}
+    <div
+      className={[styles.popup, styles.opened].join(" ")}
+      onClick={close}
+    >
+        {children}
     </div>
   );
 }
