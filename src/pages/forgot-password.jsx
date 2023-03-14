12db1391aae14f224152a7f7585202756893b2a5
margo <m.yunanova@gmail.com>
2023-03-14 12:19:03 +0300

    format reset & forgot pages

--- src/pages/forgot-password.jsx
+++ src/pages/forgot-password.jsx
@@ -1,37 +1,52 @@
-import { EmailInput, Button } from '@ya.praktikum/react-developer-burger-ui-components';
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import styles from './forgot-password.module.css';
+import {
+  Button, EmailInput
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { restorePasswordRequest } from "../utils/burger-api";
-
+import styles from "./forgot-password.module.css";
 
 const ForgotPasswordPage = () => {
   const [form, setForm] = useState({});
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    restorePasswordRequest(form)
-    .then(({success}) => {
+    restorePasswordRequest(form).then(({ success }) => {
       if (success) {
-        navigate('/reset-password', {replace: true});
-        localStorage.setItem('successResetPassword', success)
+        navigate("/reset-password", { replace: true });
+        localStorage.setItem("successResetPassword", success);
       }
       return null;
     });
-};
+  };
 
   return (
     <section className={styles.section}>
       <form className={styles.login} onSubmit={handleSubmit}>
         <h2 className="text text_type_main-medium">Восстановление пароля</h2>
-        <EmailInput name='email' placeholder='Укажите e-mail' value={form.email ?? ''} onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}/>
-        <Button htmlType="submit" type='primary' size='large'>Восстановить</Button>
+        <EmailInput
+          name="email"
+          placeholder="Укажите e-mail"
+          value={form.email ?? ""}
+          onChange={(e) =>
+            setForm({ ...form, [e.target.name]: e.target.value })
+          }
+        />
+        <Button htmlType="submit" type="primary" size="large">
+          Восстановить
+        </Button>
       </form>
       <div className={styles.info}>
         <div>
-          <span className="text text_type_main-default pr-2" >Вспомнили пароль?</span>
-          <Link className={`${styles.link} ${styles.active} text text_type_main-default`}>Войти</Link>
+          <span className="text text_type_main-default pr-2">
+            Вспомнили пароль?
+          </span>
+          <Link
+            className={`${styles.link} ${styles.active} text text_type_main-default`}
+          >
+            Войти
+          </Link>
         </div>
       </div>
     </section>
