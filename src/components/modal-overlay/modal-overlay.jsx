216fc3a358321f94a1de422befb9abda53c7d238
margo <m.yunanova@gmail.com>
2023-03-14 11:05:09 +0300

    format modalOverlay

--- src/components/modal-overlay/modal-overlay.jsx
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import { useNavigate } from "react-router";
 import styles from "./modal-overlay.module.css";
 
 export default function ModalOverlay({ children, close }) {
