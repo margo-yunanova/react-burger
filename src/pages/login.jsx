608fabe589e16375cd66cefb18bc49c9dc2ba2b9
margo <m.yunanova@gmail.com>
2023-03-14 10:59:34 +0300

    fix navigation

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,31 +1,41 @@
 import { EmailInput, PasswordInput, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, Navigate } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { authorizeUser } from '../services/actions/user';
 import styles from './login.module.css';
 
 const Login = () => {
-  const [form, setForm] = useState({})
+  const [form, setForm] = useState({});
+  const [isRequestSent, setRequestSent] = useState(false);
+  const successRequest = useSelector((state) => state.user.success);
+  const request = useSelector((state) => state.user.request);
   const dispatch = useDispatch();
-  const success = useSelector(state => state.user.success);
-  const accessToken = localStorage.getItem('accessToken');
+  const navigate = useNavigate();
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(authorizeUser(form))
-  }
-
-  if (success || accessToken) {
-    return <Navigate to={'/'} />
+    dispatch(authorizeUser(form));
+    setRequestSent(true);
   };
+
+
+  if (successRequest && !request) {
+    navigate(-1);
+    //return <Navigate to={'/'} replace={true}/>
+  };
+
+  if(!isRequestSent && request) {
+    return null
+  }
 
   return (
     <section className={styles.section}>
       <form className={styles.login} onSubmit={handleSubmit}>
         <h2 className="text text_type_main-medium">Вход</h2>
-        <EmailInput name='email' value={form.email ?? ''} onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}/>
-        <PasswordInput name='password' value={form.password ?? ''} onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}/>
+        <EmailInput name='email' value={form.email ?? ''} onChange={(e) => setForm({ ...form, [e.target.name]: e.target.value })} />
+        <PasswordInput name='password' value={form.password ?? ''} onChange={(e) => setForm({ ...form, [e.target.name]: e.target.value })} />
         <Button htmlType="submit" type='primary' size='large' width={128}>Войти</Button>
       </form>
       <div className={styles.info}>
