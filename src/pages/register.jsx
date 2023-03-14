0e474191feaa964fd6e77b7b245133f3242569ea
margo <m.yunanova@gmail.com>
2023-03-14 12:14:24 +0300

    format register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,40 +1,73 @@
-import { EmailInput, PasswordInput, Input, Button } from '@ya.praktikum/react-developer-burger-ui-components';
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, Navigate } from 'react-router-dom';
-import { registerUser } from '../services/actions/user';
-import styles from './register.module.css';
-
+import {
+  EmailInput,
+  PasswordInput,
+  Input,
+  Button,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, Navigate } from "react-router-dom";
+import { registerUser } from "../services/actions/user";
+import styles from "./register.module.css";
 
 const RegisterPage = () => {
   const [form, setForm] = useState({});
-  const success = useSelector(state => state.user.success);
-  const accessToken = localStorage.getItem('accessToken');
+  const success = useSelector((state) => state.user.success);
+  const accessToken = localStorage.getItem("accessToken");
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(registerUser(form));
-  }
+  };
 
   if (success || accessToken) {
-    return <Navigate to={'/'} />
-  };
+    return <Navigate to={"/"} />;
+  }
 
   return (
     <section className={styles.section}>
       <form className={styles.login} onSubmit={handleSubmit}>
         <h2 className="text text_type_main-medium">Регистрация</h2>
-        <Input name='name' type={'text'} placeholder={'Имя'} value={form.name ?? ''} onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}/>
-        <EmailInput name='email' value={form.email ?? ''} onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}/>
-        <PasswordInput name='password' value={form.password ?? ''} onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}/>
-        <Button htmlType="submit" type='primary' size='large'>Зарегистрироваться</Button>
+        <Input
+          name="name"
+          type={"text"}
+          placeholder={"Имя"}
+          value={form.name ?? ""}
+          onChange={(e) =>
+            setForm({ ...form, [e.target.name]: e.target.value })
+          }
+        />
+        <EmailInput
+          name="email"
+          value={form.email ?? ""}
+          onChange={(e) =>
+            setForm({ ...form, [e.target.name]: e.target.value })
+          }
+        />
+        <PasswordInput
+          name="password"
+          value={form.password ?? ""}
+          onChange={(e) =>
+            setForm({ ...form, [e.target.name]: e.target.value })
+          }
+        />
+        <Button htmlType="submit" type="primary" size="large">
+          Зарегистрироваться
+        </Button>
       </form>
       <div className={styles.info}>
         <div>
-          <span className="text text_type_main-default pr-2" >Уже зарегистрированы?</span>
-          <Link to={'/login'} className={`${styles.link} ${styles.active} text text_type_main-default`}>Войти</Link>
+          <span className="text text_type_main-default pr-2">
+            Уже зарегистрированы?
+          </span>
+          <Link
+            to={"/login"}
+            className={`${styles.link} ${styles.active} text text_type_main-default`}
+          >
+            Войти
+          </Link>
         </div>
       </div>
     </section>
