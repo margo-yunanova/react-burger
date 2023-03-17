ec30961111ffaa6b3fa93786d5f19d5ff4e50534
margo <m.yunanova@gmail.com>
2023-03-18 00:35:24 +0300

    add ProfileOrders component with styles

--- src/pages/profile-orders.jsx
+++ src/pages/profile-orders.jsx
@@ -1,5 +1,14 @@
-const ProfileOrders = () => {
+import OrderItem from "../components/order-item/order-item";
+import styles from "./profile-orders.module.css";
 
-}
+const ProfileOrders = ({ status }) => {
+  return (
+    <div className={styles.scroll}>
+      <OrderItem status={"Выполнен"} />
+      <OrderItem status={"Создан"} />
+      <OrderItem status={"Готовится"} />
+    </div>
+  );
+};
 
 export default ProfileOrders;
